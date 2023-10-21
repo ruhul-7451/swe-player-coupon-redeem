96feee317b477bfce16012ec3fd8deb5d1195cd0
Md. Ruhul Amin Khan <86850484+ruhul-7451@users.noreply.github.com>
2023-10-21 23:28:53 +0600

    business logic test cases added

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -35,4 +35,46 @@ describe('AppController (e2e)', () => {
     expect(response.body.id).toBeDefined();
     expect(response.body.value).toBeDefined();
   });
+
+  it('should handle invalid reward', async () => {
+    await request(app.getHttpServer())
+      .post('/coupon-redeem')
+      .send({ playerId: 1, rewardId: 999 }) // Non-existent reward
+      .expect(HttpStatus.NOT_FOUND);
+  });
+
+  it('should handle reward out of date range', async () => {
+    // Adjust the reward's date range to be outside the current date
+    const rewardId = 1;
+    // Set reward.endDate to a past date
+    // and set reward.startDate to a future date
+    await request(app.getHttpServer())
+      .post('/coupon-redeem')
+      .send({ playerId: 1, rewardId })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('should reject redemption when per-day limit is exceeded', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/coupon-redeem')
+      .send({ playerId: 1, rewardId: 2 }) // Reward with a perDayLimit of 1
+      .expect(HttpStatus.BAD_REQUEST);
+    expect(response.body.error).toContain('per-day limit');
+  });
+
+  it('should reject redemption when total limit is exceeded', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/coupon-redeem')
+      .send({ playerId: 1, rewardId: 3 }) // Reward with a totalLimit of 1
+      .expect(HttpStatus.BAD_REQUEST);
+    expect(response.body.error).toContain('total limit');
+  });
+
+  it('should reject redemption when the reward is not available at this time', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/coupon-redeem')
+      .send({ playerId: 1, rewardId: 4 }) // Reward with past or future dates
+      .expect(HttpStatus.BAD_REQUEST);
+    expect(response.body.error).toContain('not available at this time');
+  });
 });
